d4e604352869f583c640c533756ff4bd9ee743c6
quentingrchr <q.grancher@gmail.com>
2020-01-19 12:11:43 +0100

    waypoint library

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -2,6 +2,7 @@ const hamburger = document.querySelector("#hamburger");
 const iconMenu = document.querySelector("#icon-menu");
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll("menu ul li");
+const toAnimate = document.querySelectorAll(".to-animate");
 
 iconMenu.addEventListener("click", () => {
   hamburger.classList.toggle("menu-is-opened");
@@ -12,5 +13,15 @@ menuItems.forEach(element => {
   element.addEventListener("click", () => {
     hamburger.classList.toggle("menu-is-opened");
     menu.classList.toggle("menu-is-grown");
+  });
+});
+
+toAnimate.forEach(element => {
+  let waypoint = new Waypoint({
+    element: element,
+    handler: function() {
+      element.classList.add("move-in-bot");
+    },
+    offset: "90%"
   });
 });
